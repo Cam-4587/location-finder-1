a3708a93dfded5ee0e2b9b3f72208710436369a7
Cam-4587 Conroy <cameronconroy@live.com>
2025-05-13 15:39:18 +0000

    Finish project

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from geopy.geocoders import Nominatim
+
+def coordinates_finder():
+    geolocator = Nominatim(user_agent="my_user_agent")
+    city = input("Enter City: ")
+    while not city:
+        print("Please enter a city")
+        city = input("Enter City: ").lower()
+    country = input("Enter Country: ").lower()
+    while not country:
+        print("Please enter a country: ")
+        country = input("Enter Country: ").lower()
+    if ValueError:
+        print("\nInvalid Coordinates")
+        city = input("Enter City: ").lower()
+        while not city:
+            print("Please enter a city")
+            city = input("Enter City: ").lower()
+        country = input("Enter Country: ").lower()
+        while not country:
+            print("\nPlease enter a country: ")
+            country = input("Enter Country: ").lower()
+    loc = geolocator.geocode(city+','+ country)
+    print("\nlatitude is :-" ,loc.latitude, "|" " longtitude is:-" ,loc.longitude)
+coordinates_finder()
